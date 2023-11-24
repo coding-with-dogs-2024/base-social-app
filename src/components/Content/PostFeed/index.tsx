0a9001eb759592f42eeb9d4be2ca33dd59ba7f59
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-24 15:37:21 -0500

    Progress

--- src/components/Content/PostFeed/index.tsx
+++ src/components/Content/PostFeed/index.tsx
@@ -1,12 +1,14 @@
 import classes from './PostFeed.module.scss';
-import { useGetAllPosts } from '../../../services/jsonapi/api';
+// import { useGetAllPosts } from '../../../services/jsonapi/api';
+import { Spinner } from '../../UI/Spinner';
 
 export const PostFeed = () => {
-	useGetAllPosts();
+	// useGetAllPosts();
 	return (
 		<div className={classes.postFeed}>
 			<p>Hello</p>
 			<p>World</p>
+			<Spinner />
 		</div>
 	);
 };
