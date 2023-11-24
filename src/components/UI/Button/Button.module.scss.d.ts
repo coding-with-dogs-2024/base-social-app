1572f435c4ce2bb7b2e791b0ae64110dae123d56
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-24 15:37:22 -0500

    Moving over more files

--- src/components/UI/Button/Button.module.scss.d.ts
+++ src/components/UI/Button/Button.module.scss.d.ts
@@ -3,4 +3,5 @@ export declare const colorDefault: string;
 export declare const colorPrimary: string;
 export declare const colorSecondary: string;
 export declare const sizeDefault: string;
+export declare const sizeLarge: string;
 export declare const sizeSmall: string;
