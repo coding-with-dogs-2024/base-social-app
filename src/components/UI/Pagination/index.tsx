27404b2c6f2e9ca5e1e9381328cc2216f1a92ec7
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-24 15:37:21 -0500

    Progress with creating buttons

--- src/components/UI/Pagination/index.tsx
+++ src/components/UI/Pagination/index.tsx
@@ -1,9 +1,10 @@
 import classes from './Pagination.module.scss';
 import type { PaginationComponentProps } from './types';
+import { Button } from '../Button';
 
 export const Pagination = (props: PaginationComponentProps) => (
 	<div className={classes.pagination}>
-		<button>Previous</button>
-		<button>Next</button>
+		<Button onClick={props.previousPage}>Previous</Button>
+		<Button onClick={props.nextPage}>Next</Button>
 	</div>
 );
