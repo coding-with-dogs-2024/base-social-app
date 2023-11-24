73cf056353d87f1e201509e2e5a6208156b84cd0
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-24 15:37:21 -0500

    Added query

--- src/components/Content/PostFeed/index.tsx
+++ src/components/Content/PostFeed/index.tsx
@@ -1,6 +1,8 @@
 import classes from './PostFeed.module.scss';
+import { useGetAllPosts } from '../../../services/jsonapi/api';
 
 export const PostFeed = () => {
+	useGetAllPosts();
 	return (
 		<div className={classes.postFeed}>
 			<p>Hello</p>
