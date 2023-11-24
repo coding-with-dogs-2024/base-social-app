1bb8c2b6db58584e50d0d36651278f019b4f97b3
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-24 15:37:21 -0500

    More fixes

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,7 +1,8 @@
+import classes from './Content.module.scss';
 import { useRoutes } from 'react-router';
 import { routes } from '../../routes';
 
 export const Content = () => {
 	const Routes = useRoutes(routes);
-	return <main>{Routes}</main>;
+	return <main className={classes.content}>{Routes}</main>;
 };
