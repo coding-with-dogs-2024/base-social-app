a52bb531d796ae2aa29c8341e5183520259925ea
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-24 15:37:21 -0500

    Beginning to construct card

--- src/components/Content/PostFeed/index.tsx
+++ src/components/Content/PostFeed/index.tsx
@@ -1,6 +1,7 @@
 import classes from './PostFeed.module.scss';
 import { useGetAllPosts } from '../../../services/jsonapi/api';
 import { Spinner } from '../../UI/Spinner';
+import { Card } from '../../UI/Card';
 
 export const PostFeed = () => {
 	const { isLoading } = useGetAllPosts();
@@ -8,6 +9,9 @@ export const PostFeed = () => {
 		<div className={classes.postFeed}>
 			<h1>Post Feed</h1>
 			{isLoading && <Spinner />}
+			<div className={classes.postList}>
+				<Card title={<p>Title</p>} body={<p>Lorem Ipsum</p>} />
+			</div>
 		</div>
 	);
 };
