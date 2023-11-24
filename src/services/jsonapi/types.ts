f0791701cc797909d66f22f0b967254263b48535
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-24 15:37:22 -0500

    Type refactoring

--- src/services/jsonapi/types.ts
+++ src/services/jsonapi/types.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod';
 
 export const postSchema = z.object({
-	userId: z.number().readonly(),
-	id: z.number().readonly(),
-	title: z.string().readonly(),
-	body: z.string().readonly()
+	userId: z.number(),
+	id: z.number(),
+	title: z.string(),
+	body: z.string()
 });
 
 export type Post = z.infer<typeof postSchema>;
@@ -13,34 +13,36 @@ export const postListSchema = z.array(postSchema).readonly();
 
 export type PostList = z.infer<typeof postListSchema>;
 
-export const userSchema = z.object({
-	id: z.number().readonly(),
-	name: z.string().readonly(),
-	email: z.string().readonly(),
-	address: z
-		.object({
-			street: z.string().readonly(),
-			suite: z.string().readonly(),
-			city: z.string().readonly(),
-			zipcode: z.string().readonly(),
-			geo: z
-				.object({
-					lat: z.string().readonly(),
-					lng: z.string().readonly()
-				})
-				.readonly()
-		})
-		.readonly(),
-	phone: z.string().readonly(),
-	website: z.string().readonly(),
-	company: z
-		.object({
-			name: z.string().readonly(),
-			catchPhrase: z.string().readonly(),
-			bs: z.string().readonly()
-		})
-		.readonly()
-});
+export const userSchema = z
+	.object({
+		id: z.number(),
+		name: z.string(),
+		email: z.string(),
+		address: z
+			.object({
+				street: z.string(),
+				suite: z.string(),
+				city: z.string(),
+				zipcode: z.string(),
+				geo: z
+					.object({
+						lat: z.string(),
+						lng: z.string()
+					})
+					.readonly()
+			})
+			.readonly(),
+		phone: z.string(),
+		website: z.string(),
+		company: z
+			.object({
+				name: z.string(),
+				catchPhrase: z.string(),
+				bs: z.string()
+			})
+			.readonly()
+	})
+	.readonly();
 
 export type User = z.infer<typeof userSchema>;
 
