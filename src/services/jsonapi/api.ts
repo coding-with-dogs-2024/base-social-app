ac35ad8dc0be57aa59b69860867dd9592d84b398
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-24 15:37:21 -0500

    Added new endpoint

--- src/services/jsonapi/api.ts
+++ src/services/jsonapi/api.ts
@@ -1,10 +1,11 @@
-import type { Post, PostList } from './types';
-import { postListSchema } from './types';
+import type { Post, PostList, User } from './types';
+import { postListSchema, userSchema } from './types';
 import { useQuery } from '@tanstack/react-query';
-import type { UseQueryResult } from '@tanstack/react-query';
+import type { UseQueryResult, DefaultError } from '@tanstack/react-query';
 
 const HOST = 'https://jsonplaceholder.typicode.com';
 export const GET_ALL_POSTS = 'GET_ALL_POSTS';
+export const GET_USER_BY_ID = 'GET_USER_BY_ID';
 
 export const useGetAllPosts = (): UseQueryResult<ReadonlyArray<Post>> =>
 	useQuery<PostList>({
@@ -14,5 +15,17 @@ export const useGetAllPosts = (): UseQueryResult<ReadonlyArray<Post>> =>
 				signal
 			})
 				.then((res) => res.json())
-				.then((json) => postListSchema.parse(json))
+				.then(postListSchema.parse)
+	});
+
+type GetUserByIdQueryKey = [typeof GET_USER_BY_ID, number];
+export const useGetUserById = (userId: number): UseQueryResult<User> =>
+	useQuery<User, DefaultError, User, GetUserByIdQueryKey>({
+		queryKey: [GET_USER_BY_ID, userId],
+		queryFn: ({ signal, queryKey: [, userId] }) =>
+			fetch(`${HOST}/users/${userId}`, {
+				signal
+			})
+				.then((res) => res.json())
+				.then(userSchema.parse)
 	});
