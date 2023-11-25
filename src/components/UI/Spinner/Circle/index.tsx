f2aa41e55fe432e4401ce24411751e3915dda9a8
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-25 16:03:19 -0500

    Pulling more files over

--- src/components/UI/Spinner/Circle/index.tsx
+++ src/components/UI/Spinner/Circle/index.tsx
@@ -4,7 +4,7 @@ import classes from './Circle.module.scss';
  * Got this design from https://loading.io/css/
  */
 export const CircleSpinner = () => (
-	<div className={classes.ldsRoller}>
+	<div role="progressbar" className={classes.ldsRoller}>
 		<div></div>
 		<div></div>
 		<div></div>
