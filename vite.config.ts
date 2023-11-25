e1319e76bb38bd707627b1b9fc2cd9ecede03773
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-25 16:03:19 -0500

    Setting up test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
+// noinspection JSAnnotator
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
 	root: path.join(process.cwd(), 'src'),
+	test: {
+		root: path.join(process.cwd(), 'test')
+	},
 	css: {
 		modules: {
 			localsConvention: 'camelCase'
