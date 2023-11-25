3107c22b988c99d338908a260b37df571fce67b5
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-25 16:05:37 -0500

    Test progress

--- test/components/Content/PostFeed/PostFeed.test.tsx
+++ test/components/Content/PostFeed/PostFeed.test.tsx
@@ -2,8 +2,24 @@ import { render, screen } from '@testing-library/react';
 import { InjectablePostFeed } from '../../../../src/components/Content/PostFeed';
 import type { MockedFunction } from 'vitest';
 import { useGetAllPosts } from '../../../../src/services/jsonapi/api';
+import type { PostList } from '../../../../src/services/jsonapi/types';
 
 const useGetAllPostsMock: MockedFunction<typeof useGetAllPosts> = vi.fn();
+
+const posts: PostList = [
+	{
+		id: 1,
+		userId: 1,
+		title: 'First Post',
+		body: 'This is the first post'
+	},
+	{
+		id: 2,
+		userId: 1,
+		title: 'Second Post',
+		body: 'This is the second post'
+	}
+];
 
 describe('PostFeed', () => {
 	beforeEach(() => {
@@ -26,5 +42,18 @@ describe('PostFeed', () => {
 		expect(screen.queryAllByTestId('post-card')).toHaveLength(0);
 	});
 
-	it.fails('shows post list from data');
+	it('shows post list from data', () => {
+		useGetAllPostsMock.mockReturnValue({
+			isLoading: false,
+			data: posts
+		});
+		render(<InjectablePostFeed useGetAllPosts={useGetAllPostsMock} />);
+
+		expect(
+			screen.getByRole('heading', {
+				name: 'Post Feed'
+			})
+		).toBeVisible();
+		expect(screen.queryByRole('progressbar')).not.toBeInTheDocument();
+	});
 });
