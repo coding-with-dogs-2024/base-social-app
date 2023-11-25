4dfc40627f6743282b9239891bdddfb97ef5d2a2
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-25 16:05:37 -0500

    Working on test logic

--- src/components/Content/PostFeed/index.tsx
+++ src/components/Content/PostFeed/index.tsx
@@ -1,5 +1,8 @@
 import classes from './PostFeed.module.scss';
-import { useGetAllPosts } from '../../../services/jsonapi/api';
+import {
+	useGetAllPosts,
+	useGetAllPosts as useGetAllPostsDefault
+} from '../../../services/jsonapi/api';
 import { CircleSpinner } from '../../UI/Spinner/Circle';
 import { PostCard } from '../../PostCard';
 import { useEffect, useMemo } from 'react';
@@ -7,7 +10,48 @@ import { usePagination } from '../../UI/Pagination/usePagination';
 import type { Post } from '../../../services/jsonapi/types';
 import { Pagination } from '../../UI/Pagination';
 
+type Props = Readonly<{
+	useGetAllPosts?: typeof useGetAllPostsDefault;
+}>;
+
 export const PostFeed = () => {
+	const { updatePagination, extractPage, componentProps } =
+		usePagination<Post>(10);
+	const { isLoading, data } = useGetAllPosts();
+
+	useEffect(() => {
+		if (data) {
+			updatePagination(data);
+		}
+	}, [data, updatePagination]);
+
+	const dataPage = useMemo(() => {
+		if (!data) {
+			return [];
+		}
+		return extractPage(data);
+	}, [data, extractPage]);
+
+	return (
+		<div className={classes.postFeed}>
+			<h1>Post Feed</h1>
+			{isLoading && <CircleSpinner />}
+			{!isLoading && (
+				<div className={classes.postList}>
+					{data &&
+						dataPage.map((post) => (
+							<PostCard key={post.id} post={post} />
+						))}
+					<Pagination {...componentProps} />
+				</div>
+			)}
+		</div>
+	);
+};
+
+export const InjectablePostFeed = ({
+	useGetAllPosts = useGetAllPostsDefault
+}: Props) => {
 	const { updatePagination, extractPage, componentProps } =
 		usePagination<Post>(10);
 	const { isLoading, data } = useGetAllPosts();
