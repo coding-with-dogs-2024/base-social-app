5faf3ba1cbe96f006fec6f4be1d2b112526b4544
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-25 16:03:19 -0500

    Building out user info

--- src/components/Content/UserProfile/UserInfo/index.tsx
+++ src/components/Content/UserProfile/UserInfo/index.tsx
@@ -1,17 +1,36 @@
 import type { User } from '../../../../services/jsonapi/types';
 import classes from './UserInfo.module.scss';
 
-type Props = Readonly<{
+type UserInfoProps = Readonly<{
 	user: User;
 }>;
 
-export const UserInfo = (props: Props) => (
-	<div className={classes.userInfo}>
-		<div className={classes.row}>
-			<p>
-				<strong>Email:</strong>
-			</p>
-			<p>{props.user.email}</p>
-		</div>
+type RowProps = Readonly<{
+	label: string;
+	value: string;
+}>;
+
+const Row = (props: RowProps) => (
+	<div className={classes.row}>
+		<p>
+			<strong>{props.label}:</strong>
+		</p>
+		<p>{props.value}</p>
 	</div>
 );
+
+export const UserInfo = (props: UserInfoProps) => {
+	const {
+		address: { street, suite, city, zipcode }
+	} = props.user;
+	const addressString = `${street}, ${suite}, ${city} ${zipcode}`;
+	return (
+		<div className={classes.userInfo}>
+			<Row label="Email" value={props.user.email} />
+			<Row label="Address" value={addressString} />
+			<Row label="Phone" value={props.user.phone} />
+			<Row label="Website" value={props.user.website} />
+			<Row label="Company" value={props.user.company.name} />
+		</div>
+	);
+};
