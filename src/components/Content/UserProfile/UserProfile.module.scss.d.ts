688196d7e9571aa032e723216b963cf1b71c504f
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-25 16:03:19 -0500

    Working on user profile

--- src/components/Content/UserProfile/UserProfile.module.scss.d.ts
+++ src/components/Content/UserProfile/UserProfile.module.scss.d.ts
@@ -1,1 +1,2 @@
+export declare const profileTitle: string;
 export declare const userProfile: string;
--- src/components/Content/UserProfile/index.tsx
+++ src/components/Content/UserProfile/index.tsx
@@ -2,6 +2,7 @@ import classes from './UserProfile.module.scss';
 import { useParams } from 'react-router';
 import { useGetUserById } from '../../../services/jsonapi/api';
 import { useMemo } from 'react';
+import { CircleSpinner } from '../../UI/Spinner/Circle';
 
 type Params = Readonly<{
 	id: string;
@@ -19,10 +20,14 @@ const useGetUserId = (): number => {
 
 export const UserProfile = () => {
 	const userId = useGetUserId();
-	useGetUserById(userId);
+	const { isLoading, data } = useGetUserById(userId);
 	return (
 		<div className={classes.userProfile}>
-			<h1>User Profile</h1>
+			<div className={classes.profileTitle}>
+				<h1>User Profile</h1>
+				{data && <h2>{data.name}</h2>}
+			</div>
+			{isLoading && <CircleSpinner />}
 		</div>
 	);
 };
