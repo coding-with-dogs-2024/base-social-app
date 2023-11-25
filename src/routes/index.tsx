7e802ec2b74e33226a344b65829ae52e5f48004f
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-25 16:03:18 -0500

    Made top level pages lazy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,25 @@
 import type { RouteObject } from 'react-router';
-import { PostFeed } from '../components/Content/PostFeed';
 import { Navigate } from 'react-router';
+import { namedLazy } from '../utils/namedLazy';
+
+const PostFeed = namedLazy(
+	() => import('../components/Content/PostFeed'),
+	'PostFeed'
+);
+
+const UserProfile = namedLazy(
+	() => import('../components/Content/UserProfile'),
+	'UserProfile'
+);
 
 export const routes: RouteObject[] = [
 	{
 		path: '/post-feed',
 		element: <PostFeed />
+	},
+	{
+		path: '/user/:id',
+		element: <UserProfile />
 	},
 	{
 		path: '*',
