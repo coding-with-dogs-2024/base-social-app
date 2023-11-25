11168a562004efbb9adde3afb0eecba7c72119dc
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-25 16:05:37 -0500

    Stubbed out tests

--- src/components/Content/PostFeed/index.tsx
+++ src/components/Content/PostFeed/index.tsx
@@ -29,13 +29,15 @@ export const PostFeed = () => {
 		<div className={classes.postFeed}>
 			<h1>Post Feed</h1>
 			{isLoading && <CircleSpinner />}
-			<div className={classes.postList}>
-				{data &&
-					dataPage.map((post) => (
-						<PostCard key={post.id} post={post} />
-					))}
-				<Pagination {...componentProps} />
-			</div>
+			{!isLoading && (
+				<div className={classes.postList}>
+					{data &&
+						dataPage.map((post) => (
+							<PostCard key={post.id} post={post} />
+						))}
+					<Pagination {...componentProps} />
+				</div>
+			)}
 		</div>
 	);
 };
---
+++ test/components/Content/PostFeed/PostFeed.test.tsx
@@ -0,0 +1,5 @@
+describe('PostFeed', () => {
+	it.fails('shows loading spinner when waiting on posts to load');
+
+    it.fails('shows post list from data');
+});
