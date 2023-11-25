f9dd377f1621e12e3a1db0f4536f51ad575844bf
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-25 16:03:18 -0500

    Working on spinner

--- src/components/Content/PostFeed/PostCard/index.tsx
+++ src/components/Content/PostFeed/PostCard/index.tsx
@@ -1,10 +1,20 @@
 import type { Post } from '../../../../services/jsonapi/types';
 import { Card } from '../../../UI/Card';
 import classes from './PostCard.module.scss';
+import {useGetUserById} from '../../../../services/jsonapi/api';
 
 type Props = Readonly<{
 	post: Post;
 }>;
+
+const PostTitle = (props: Props) => {
+	const { isLoading, data } = useGetUserById(props.post.userId);
+	return (
+		<div className={classes.title}>
+			{props.post.id} - {props.post.title}
+		</div>
+	);
+};
 
 export const PostCard = (props: Props) => {
 	return (
---
+++ src/components/UI/Spinner/Ellipsis/index.tsx
@@ -0,0 +1,10 @@
+import classes from './Ellipsis.module.scss';
+
+export const EllipsisSpinner = () => (
+	<div className={classes.ldsEllipsis}>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+	</div>
+);
