35b77bbb8ca1bda07d224937cad06014e169fa37
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-25 16:03:18 -0500

    Fixed some query stuff

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,23 @@
 import { Navbar } from './Navbar';
 import { Content } from './Content';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			gcTime: 0,
+			staleTime: 300_000
+		}
+	}
+});
 
 export const App = () => (
 	<BrowserRouter>
-		<Navbar />
-		<Content />
+		<QueryClientProvider client={queryClient}>
+			<Navbar />
+			<Content />
+		</QueryClientProvider>
 	</BrowserRouter>
 );
