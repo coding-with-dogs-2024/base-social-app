ba01dffd7dfee3db8f60da7cc8a9205f8627aad3
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-25 16:05:36 -0500

    More progress

--- test/components/UI/Pagination/Pagination.test.tsx
+++ test/components/UI/Pagination/Pagination.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { Pagination } from '../../../../src/components/UI/Pagination';
 import type { PaginationComponentProps } from '../../../../src/components/UI/Pagination/types';
 import type { MockedFunction } from 'vitest';
+import { userEvent } from '@testing-library/user-event';
 
 type DoRenderProps = Omit<
 	PaginationComponentProps,
@@ -34,5 +35,50 @@ describe('Pagination', () => {
 
 		const buttons = screen.queryAllByRole('button');
 		expect(buttons).toHaveLength(0);
+	});
+
+	it('renders with previous page button only', async () => {
+		doRender({
+			showPreviousPage: true,
+			showNextPage: false
+		});
+
+		const button = screen.getByRole('button');
+		expect(button).toHaveTextContent('Previous');
+		await userEvent.click(button);
+
+		expect(previousPage).toHaveBeenCalledOnce();
+	});
+
+	it('renders with next page button only', async () => {
+		doRender({
+			showPreviousPage: false,
+			showNextPage: true
+		});
+
+		const button = screen.getByRole('button');
+		expect(button).toHaveTextContent('Next');
+		await userEvent.click(button);
+
+		expect(nextPage).toHaveBeenCalledOnce();
+	});
+
+	it('renders with both buttons', async () => {
+		doRender({
+			showPreviousPage: true,
+			showNextPage: true
+		});
+
+		const previousButton = screen.getByRole('button', {
+			name: 'Previous'
+		});
+		await userEvent.click(previousButton);
+		expect(previousButton).toHaveBeenCalledOnce();
+
+		const nextButton = screen.getByRole('button', {
+			name: 'Next'
+		});
+		await userEvent.click(nextButton);
+		expect(nextButton).toHaveBeenCalledOnce();
 	});
 });
