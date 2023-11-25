166d86346fd17e7f5768d5a76e7245ad542d5dad
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-25 16:05:38 -0500

    Updating tests

--- test/components/Content/Registration/Registration.test.tsx
+++ test/components/Content/Registration/Registration.test.tsx
@@ -1,5 +1,21 @@
-describe('Registration', () => {
-	it.fails('renders all the controls');
+import { render, screen } from '@testing-library/react';
+import { Registration } from '../../../../src/components/Content/Registration';
 
-	it.fails('can update all the controls');
+describe('Registration', () => {
+	it('can update all the controls', async () => {
+		render(<Registration />);
+		expect(
+			screen.getByRole('heading', {
+				level: 1
+			})
+		).toHaveTextContent('Registration');
+
+		const sectionHeadings = screen.getAllByRole('heading', {
+			level: 2
+		});
+		expect(sectionHeadings).toHaveLength(3);
+		expect(sectionHeadings[0]).toHaveTextContent('Credentials');
+		expect(sectionHeadings[1]).toHaveTextContent('Personal Information');
+		expect(sectionHeadings[2]).toHaveTextContent('App Settings');
+	});
 });
