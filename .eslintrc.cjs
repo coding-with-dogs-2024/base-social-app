1e9e0e3ebc83721f12d632fccc2df35e4ec95aad
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-25 16:03:18 -0500

    Fixing more eslint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,42 +1,53 @@
 module.exports = {
-    extends: [
-        'eslint:recommended',
-        'plugin:prettier/recommended',
-        'plugin:import/recommended',
-        'plugin:@tanstack/eslint-plugin-query/recommended',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:jsx-a11y/recommended',
-        'plugin:react/jsx-runtime'
-    ],
-    parserOptions: {
-        ecmaVersion: 2022,
-        ecmaFeatures: {
-            jsx: true
-        }
-    },
-    rules: {
-        'no-console': [
-            'error',
-            {
-                allow: ['error']
-            }
-        ],
-        'prettier/prettier': ['error', {}, { usePrettierrc: true }]
-    },
-    overrides: [
-        {
-            files: ['**/*.{ts,tsx,mts,cts}'],
-            parser: '@typescript-eslint/parser',
-            extends: [
-                'plugin:@typescript-eslint/recommended',
-                'plugin:import/typescript'
-            ],
-            settings: {
-                'import/resolver': {
-                    typescript: {}
-                }
-            }
-        }
-    ]
-}
+	extends: [
+		'eslint:recommended',
+		'plugin:prettier/recommended',
+		'plugin:import/recommended',
+		'plugin:@tanstack/eslint-plugin-query/recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:react/jsx-runtime'
+	],
+	parserOptions: {
+		ecmaVersion: 2022,
+		ecmaFeatures: {
+			jsx: true
+		}
+	},
+	rules: {
+		'no-console': [
+			'error',
+			{
+				allow: ['error']
+			}
+		],
+		'prettier/prettier': ['error', {}, { usePrettierrc: true }]
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	},
+	overrides: [
+		{
+			files: ['**/*.{ts,tsx,mts,cts}'],
+			parser: '@typescript-eslint/parser',
+			extends: [
+				'plugin:@typescript-eslint/recommended',
+				'plugin:import/typescript'
+			],
+			settings: {
+				'import/resolver': {
+					typescript: {}
+				}
+			}
+		},
+		{
+			files: ['**/*.cjs'],
+			env: {
+				node: true
+			}
+		}
+	]
+};
