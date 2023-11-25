02009b59beb8f042bcd151552ca4709c2af76307
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-25 16:03:18 -0500

    More progress

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,10 @@
 import classes from './Navbar.module.scss';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => (
 	<header className={classes.navbar}>
-		<h1 className={classes.title}>My Social App</h1>
+		<h1 className={classes.title}>
+			<Link to="/">My Social App</Link>
+		</h1>
 	</header>
 );
