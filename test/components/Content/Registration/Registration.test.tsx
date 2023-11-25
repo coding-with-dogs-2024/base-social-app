f5444e39374b901e81aeb1e745fab524671a9190
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-25 16:05:38 -0500

    More progress with tests

--- test/components/Content/Registration/Registration.test.tsx
+++ test/components/Content/Registration/Registration.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { Registration } from '../../../../src/components/Content/Registration';
+import { userEvent } from '@testing-library/user-event';
 
 describe('Registration', () => {
 	it('can update all the controls', async () => {
@@ -17,5 +18,26 @@ describe('Registration', () => {
 		expect(sectionHeadings[0]).toHaveTextContent('Credentials');
 		expect(sectionHeadings[1]).toHaveTextContent('Personal Information');
 		expect(sectionHeadings[2]).toHaveTextContent('App Settings');
+
+		const emailInput = screen.getByLabelText('Email');
+		await userEvent.type(emailInput, 'user@gmail.com');
+		expect(emailInput).toHaveValue('user@gmail.com');
+
+		const passwordInput = screen.getByLabelText('Password');
+		await userEvent.type(passwordInput, 'password');
+		expect(passwordInput).toHaveValue('password');
+
+		const firstNameInput = screen.getByLabelText('First Name');
+		await userEvent.type(firstNameInput, 'John');
+		expect(firstNameInput).toHaveValue('John');
+
+		const lastNameInput = screen.getByLabelText('Last Name');
+		await userEvent.type(lastNameInput, 'Doe');
+		expect(lastNameInput).toHaveValue('Doe');
+
+		const dateOfBirthInput = screen.getByLabelText('Date of Birth');
+		// Point out this is counter-intuitive
+		await userEvent.type(dateOfBirthInput, '2020-01-01');
+		expect(dateOfBirthInput).toHaveValue('2020-01-01');
 	});
 });
