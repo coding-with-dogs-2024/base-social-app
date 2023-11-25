fa6b296a60974095f6ce919e4a5d140dc46a486d
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-25 16:05:37 -0500

    Working on state

--- src/components/Content/Registration/index.tsx
+++ src/components/Content/Registration/index.tsx
@@ -10,48 +10,61 @@ const GENDER_OPTIONS: ReadonlyArray<RadioOption> = [
 	{ value: 'other', label: 'Other' }
 ];
 
-export const Registration = () => (
-	<div className={classes.registration}>
-		<h1>Registration</h1>
-		<section>
-			<h2>Credentials</h2>
-			<div className={classes.row}>
-				<Input type="text" name="email" labelText="Email" />
-				<Input type="password" name="password" labelText="Password" />
-			</div>
-		</section>
-		<section>
-			<h2>Personal Information</h2>
-			<div className={classes.row}>
-				<Input type="text" name="firstName" labelText="First Name" />
-				<Input type="text" name="lastName" labelText="Last Name" />
-			</div>
-			<div className={classes.row}>
-				<Input
-					type="date"
-					name="dateOfBirth"
-					labelText="Date of Birth"
-				/>
-				<Radio
-					labelText="Gender"
-					name="gender"
-					options={GENDER_OPTIONS}
-				/>
-			</div>
-		</section>
-		<section>
-			<h2>App Settings</h2>
-			<div className={classes.row}>
-				<Checkbox
-					name="notifications"
-					labelText="Receive Notifications?"
-				/>
-				<Input
-					type="number"
-					name="postLimit"
-					labelText="Daily Post Limit"
-				/>
-			</div>
-		</section>
-	</div>
-);
+type State = Readonly<{
+	firstName: string;
+	lastName: string;
+	email: string;
+	password: string;
+	dateOfBirth: string;
+	gender: 'male' | 'female' | 'other';
+	notifications: boolean;
+	dailyPostLimit: number;
+}>;
+
+export const Registration = () => {
+	return (
+		<div className={classes.registration}>
+			<h1>Registration</h1>
+			<section>
+				<h2>Credentials</h2>
+				<div className={classes.row}>
+					<Input type="text" name="email" labelText="Email" />
+					<Input type="password" name="password" labelText="Password" />
+				</div>
+			</section>
+			<section>
+				<h2>Personal Information</h2>
+				<div className={classes.row}>
+					<Input type="text" name="firstName" labelText="First Name" />
+					<Input type="text" name="lastName" labelText="Last Name" />
+				</div>
+				<div className={classes.row}>
+					<Input
+						type="date"
+						name="dateOfBirth"
+						labelText="Date of Birth"
+					/>
+					<Radio
+						labelText="Gender"
+						name="gender"
+						options={GENDER_OPTIONS}
+					/>
+				</div>
+			</section>
+			<section>
+				<h2>App Settings</h2>
+				<div className={classes.row}>
+					<Checkbox
+						name="notifications"
+						labelText="Receive Notifications?"
+					/>
+					<Input
+						type="number"
+						name="postLimit"
+						labelText="Daily Post Limit"
+					/>
+				</div>
+			</section>
+		</div>
+	);
+}
